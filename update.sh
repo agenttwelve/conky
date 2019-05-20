c14e84dff46e80e66e31368512d8e6650c8a5d0b
Erik Koch <ekoch@ekoch.local>
2019-05-20 16:46:25 -0400

    v1.0

--- update.sh
+++ update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd /home/agenttwelve/conky
 echo "Checking GitHub for updates"
 sudo git pull
-echo "updating Conky Configuration"
+echo "Updating Conky configuration"
 sudo cp -rf /home/agenttwelve/conky/conky.conf /etc/conky
