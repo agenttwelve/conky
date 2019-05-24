708a8a6446ad1629b23219fb2fb6fdb8235233c8
Erik Koch <ekoch@dhcp-3-141.corp.edenfoods.com>
2019-05-24 16:39:06 -0400

    v1.2.1

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 gpu=$(test -f /etc/X11/xorg.conf; echo $?);
 if [ "$gpu" == 1 ]; then
   echo Dedicated GPU not detected
-  ( "~/conky/scripts/nogpu.sh" )
+  ( "scripts/nogpu.sh" )
 else
   echo Dedicated GPU found
-  ( "~/conky/scripts/gpu.sh" )
+  ( "scripts/gpu.sh" )
 fi;
