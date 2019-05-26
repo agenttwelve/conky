52dd9440348fa4e5b611e27c7a60c2ce20210682
Erik Koch <agenttw3lve@gmail.com>
2019-05-26 16:46:30 -0400

    v1.3.1

--- install.sh
+++ install.sh
@@ -4,23 +4,23 @@ gpu=$(test -f /etc/X11/xorg.conf; echo $?);
 os=$(lsb_release -d | cut -c14-)
 
 if [[ "$os" =~ "Pop!" ]]; then
- sudo cp -rf /assets/popos-logo.png /etc/conky/os-logo.png && echo Pop!_OS Detected
+ sudo cp -rf assets/popos-logo.png /etc/conky/os-logo.png && echo Pop!_OS Detected
 elif [[ "$os" =~ "Manj" ]]; then
-  sudo cp -rf /assets/manjaro-logo.png /etc/conky/os-logo.png && echo Manjaro Detected
+  sudo cp -rf assets/manjaro-logo.png /etc/conky/os-logo.png && echo Manjaro Detected
 elif [[ "$os" =~ "Ubuntu" ]]; then
-  sudo cp -rf /assets/ubuntu-logo.png /etc/conky/os-logo.png && echo Ubuntu Detected
+  sudo cp -rf assets/ubuntu-logo.png /etc/conky/os-logo.png && echo Ubuntu Detected
 elif [[ "$os" =~ "Debian" ]]; then
-  sudo cp -rf /assets/debian-logo.png /etc/conky/os-logo.png && echo Debian Detected
+  sudo cp -rf assets/debian-logo.png /etc/conky/os-logo.png && echo Debian Detected
 elif [[ "$os" =~ "Mint" ]]; then
-  sudo cp -rf /assets/mint-logo.png /etc/conky/os-logo.png && echo Linux Mint Detected
+  sudo cp -rf assets/mint-logo.png /etc/conky/os-logo.png && echo Linux Mint Detected
 elif [[ "$os" =~ "Arch" ]]; then
-  sudo cp -rf /assets/arch-logo.png /etc/conky/os-logo.png && echo Arch Linux Detected
+  sudo cp -rf assets/arch-logo.png /etc/conky/os-logo.png && echo Arch Linux Detected
 elif [[ "$os" =~ "Gentoo" ]]; then
-  sudo cp -rf /assets/gentoo-logo.png /etc/conky/os-logo.png && echo Gentoo Detected
+  sudo cp -rf assets/gentoo-logo.png /etc/conky/os-logo.png && echo Gentoo Detected
 elif [[ "$os" =~ "Puppy" ]]; then
-  sudo cp -rf /assets/puppy-logo.png /etc/conky/os-logo.png && echo Puppy Linux Detected
+  sudo cp -rf assets/puppy-logo.png /etc/conky/os-logo.png && echo Puppy Linux Detected
 elif [[ "$os" =~ "Raspbian" ]]; then
-  sudo cp -rf /assets/raspbian-logo.png /etc/conky/os-logo.png && echo Raspbian Detected
+  sudo cp -rf assets/raspbian-logo.png /etc/conky/os-logo.png && echo Raspbian Detected
 fi;
 
 
