ff138598c8480601ed6abe21f636e3ca06f56636
Erik Koch <ekoch@dhcp-3-141.corp.edenfoods.com>
2019-05-28 13:54:51 -0400

    v1.3.4 - Dynamic active interface and IPs!

--- scripts/gpu.sh
+++ scripts/gpu.sh
@@ -5,23 +5,23 @@ net=$(route | grep '^default' | grep -o '[^ ]*$');
 
 if [ "$cores" == 1 ]; then
   echo One CPU detected...
-  sudo cp -rf ~/conky/configs/gpu/gpu1core.conf /etc/conky/conky.conf && echo Updated Configuration
+  sudo cp -rf ~/conky/configs/gpu/gpu1core.conf /etc/conky/conky.conf && echo Updated configuration
 
 elif [ "$cores" == 2 ]; then
   echo Two CPUs detected
-  sudo cp -rf ~/conky/configs/gpu/gpu2core.conf /etc/conky/conky.conf && echo Updated Configuration
+  sudo cp -rf ~/conky/configs/gpu/gpu2core.conf /etc/conky/conky.conf && echo Updated configuration
 
 elif [ "$cores" == 4 ]; then
   echo Four CPUs detected
-  sudo cp -rf ~/conky/configs/gpu/gpu4core.conf /etc/conky/conky.conf && echo Updated Configuration
+  sudo cp -rf ~/conky/configs/gpu/gpu4core.conf /etc/conky/conky.conf && echo Updated configuration
 
 elif [ "$cores" == 8 ]; then
   echo Eight CPUs detected
-  sudo cp -rf ~/conky/configs/gpu/gpu8core.conf /etc/conky/conky.conf && echo Updated Configuration
+  sudo cp -rf ~/conky/configs/gpu/gpu8core.conf /etc/conky/conky.conf && echo Updated configuration
 
 elif [ "$cores" == 12 ]; then
   echo Twelve CPUs detected
-  sudo cp -rf ~/conky/configs/gpu/gpu12core.conf /etc/conky/conky.conf && echo Updated Configuration
+  sudo cp -rf ~/conky/configs/gpu/gpu12core.conf /etc/conky/conky.conf && echo Updated configuration
 
 fi;
 
--- scripts/nogpu.sh
+++ scripts/nogpu.sh
@@ -5,23 +5,23 @@ net=$(route | grep '^default' | grep -o '[^ ]*$');
 
 if [ "$cores" == 1 ]; then
   echo One CPU detected...
-  sudo cp -rf ~/conky/configs/nogpu/nogpu1core.conf /etc/conky/conky.conf && echo Updated Configuration
+  sudo cp -rf ~/conky/configs/nogpu/nogpu1core.conf /etc/conky/conky.conf && echo Updated configuration
 
 elif [ "$cores" == 2 ]; then
   echo Two CPUs detected
-  sudo cp -rf ~/conky/configs/nogpu/nogpu2core.conf /etc/conky/conky.conf && echo Updated Configuration
+  sudo cp -rf ~/conky/configs/nogpu/nogpu2core.conf /etc/conky/conky.conf && echo Updated configuration
 
 elif [ "$cores" == 4 ]; then
   echo Four CPUs detected
-  sudo cp -rf ~/conky/configs/nogpu/nogpu4core.conf /etc/conky/conky.conf && echo Updated Configuration
+  sudo cp -rf ~/conky/configs/nogpu/nogpu4core.conf /etc/conky/conky.conf && echo Updated configuration
 
 elif [ "$cores" == 8 ]; then
   echo Eight CPUs detected
-  sudo cp -rf ~/conky/configs/nogpu/nogpu8core.conf /etc/conky/conky.conf && echo Updated Configuration
+  sudo cp -rf ~/conky/configs/nogpu/nogpu8core.conf /etc/conky/conky.conf && echo Updated configuration
 
 elif [ "$cores" == 12 ]; then
   echo Twelve CPUs detected
-  sudo cp -rf ~/conky/configs/nogpu/nogpu12core.conf /etc/conky/conky.conf && echo Updated Configuration
+  sudo cp -rf ~/conky/configs/nogpu/nogpu12core.conf /etc/conky/conky.conf && echo Updated configuration
 
 fi;
 
